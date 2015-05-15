1336fc52a1983951fd2c4ddbcbb69b043dfb782e
Andrey Kostromin <andreykostromin@gmail.com>
2015-05-15 20:07:31 +0300

    validate responce - authorization; add usage; refactoring

--- acquia-curl.php
+++ acquia-curl.php
@@ -4,137 +4,188 @@
  * Acquia Cloud API v1
  * First usage "./acquia-curl.php reset" to create cache
  * To view usage run "./acquia-curl.php"
- */ 
+ */
 
-// Parse args and init.
-$script_name = array_shift($argv);
-if($argc < 2) {
-	echo "Usage: {$script_name} <command> domain1 domain2 ...\n";
-	// TODO: list available commands
-	exit(1);
-}
-
-$command = array_shift($argv);
+// Init.
 $c = new AcquiaCurlCommand();
-// TODO: check for options (args which started from -)
-if (empty($argv)) {
-	$c->execute($command);
+
+// Validate argument count
+$script_name = array_shift($argv);
+if ($argc < 2) {
+    echo "Usage: {$script_name} <command> domain1 domain2 ...\n";
+    // List available commands.
+    $usages = $c->getAllowedCommands();
+    foreach ($usages as $usage) {
+        printf($usage . "\n", $script_name);
+    }
+    exit(1);
 }
-else {
-	foreach ($argv as $arg) {
-		$c->execute($command, $arg);
-	}
+
+// Parse args and options
+$command = array_shift($argv);
+$args = array();
+$options = array();
+foreach ($argv as $arg) {
+    // If arg starts from "-" then it is option.
+    if (strlen($arg) > 1 && strpos($arg, '-') === 0) {
+        $options[] = $arg;
+    }
+    else {
+        $args[] = $arg;
+    }
 }
 
+// Execute command
+if (empty($args)) {
+    $c->execute($command, NULL, $options);
+} else {
+    foreach ($args as $arg) {
+        $c->execute($command, $arg, $options);
+    }
+}
 
-
-
+/**
+ * Base class for Acquia cloud API.
+ * Uses https://cloudapi.acquia.com/v1/.
+ */
 class AcquiaCurl {
-	// Settings
-	protected $endpoint = 'https://cloudapi.acquia.com/v1/';
-	protected $username;
-	protected $token;
-	protected $c;
-	protected $debug;
+    // Settings.
+    protected $endpoint = 'https://cloudapi.acquia.com/v1/';
+    protected $curl;
+    protected $debug = 0;
+    protected $settings = array();
+    
+    const NOT_AUTORIZED = 'Not authorized';
 
-	function __construct() {
-		$settings = parse_ini_file("acquia-curl.ini");
-		$this->username = $settings['username'];
-		$this->token = $settings['token'];
-		$this->debug = $settings['debug'];
+    function __construct() {
+        // Read settings.
+        $this->settings = parse_ini_file("acquia-curl.ini");
+        $this->debug = $this->settings['debug'];
+        $username = $this->settings['username'];
+        $token = $this->settings['token'];
 
-		$this->c = curl_init();
-		curl_setopt($this->c, CURLOPT_RETURNTRANSFER, true);
-		curl_setopt($this->c, CURLOPT_USERPWD, "{$this->username}:{$this->token}");
-	}
+        // Init curl.
+        $this->curl = curl_init();
+        curl_setopt($this->curl, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($this->curl, CURLOPT_USERPWD, "{$username}:{$token}");
+    }
 
-	public function fetch($command) {
-		if ($this->debug) {
-			print "Execute: {$this->endpoint}{$command}.json ...\n";
-		}
-		curl_setopt($this->c, CURLOPT_URL, "{$this->endpoint}{$command}.json");
-	        $resp = curl_exec($this->c);
-		if ($this->debug) {
-			print $resp . "\n";
-		}
-		$result = json_decode($resp, TRUE);
-		return $result;
-	}
+    public function fetch($command) {
+        if ($this->debug > 0) {
+            print "Execute: {$this->endpoint}{$command}.json ...\n";
+        }
+        
+        // Send request and fetch responce.
+        curl_setopt($this->curl, CURLOPT_URL, "{$this->endpoint}{$command}.json");
+        $resp = curl_exec($this->curl);
+        $result = json_decode($resp, TRUE);
+        
+        // Validate result.
+        if (isset($result['message']) && $result['message'] == self::NOT_AUTORIZED) {
+            print "Error: " . self::NOT_AUTORIZED . ". Please check username and token\n";
+        }
+        
+        if ($this->debug > 1) {
+            print $resp . "\n";
+        }
+        
+        return $result;
+    }
+    
+    protected function _parse_domain($str) {
+        // Strip https:// and "http://"
+        if (strpos($str, '//' !== FALSE)) {
+            $str = parse_url($str, PHP_URL_HOST);
+        } 
+        else {
+            $parts = explode("/", $str);
+            $str = $parts[0];
+        }
+        return $str;
+    }
 }
 
+/**
+ * Implements https://cloudapi.acquia.com/
+ */
 class AcquiaCurlCommand extends AcquiaCurl {
-	protected $cache_file = 'acquia-curl.json';
-	protected $cache = array();
+    protected $cache = array();
+    protected $allowed_commands = array(
+        'reset' => 'Build domain cache. Run it in first and after domain add/delete. Usage: %s reset',
+        'view' => 'View current cache. Usage: %s view',
+        'varnish' => 'Reset varnish for domains. Usage: %s varnish domain1 domain2 ...',
+    );
+    protected $allowed_options = array(
+        '-v' => 'Enable debug mode. Override value from config.',
+        '-h' => 'Print default help',
+    );
 
-	protected $commands = array(
-		'reset' => 'Build domain cache. Run it in first and after domain add/delete',
-		'view' => 'View current cache',
-		'varnish' => 'Reset varnish for domains. Usage: ./acquia-curl.php varnish domain1 domain2 ...',
-	);
+    function __construct() {
+        parent::__construct();
+        if (is_readable($this->settings['cache_file'])) {
+            $json = file_get_contents($this->settings['cache_file']);
+            $this->cache = json_decode($json, TRUE);
+        } else {
+            print "Cache file is not readable or does not exist\n";
+        }
+    }
+    
+    public function getAllowedCommands() {
+        return $this->allowed_commands;
+    }
 
-	function __construct() {
-		parent::__construct();
-		if (is_readable($this->cache_file)) {
-			$json = file_get_contents($this->cache_file);
-			$this->cache = json_decode($json, TRUE);
-		}
-		else {
-			print  "Cache file is not readable or does not exist\n";
-		}
-	}
+    public function execute($command, $arg = '', $options = array()) {
+        if (in_array($command, array_keys($this->allowed_commands))) {
+            $this->$command($arg);
+        }
+    }
 
-	public function execute($command, $args = array()) {
-		if (in_array($command, array_keys($this->commands))) {
-			$this->$command($args);
-		}
-	}
+    protected function reset($args = array()) {
+        $sites = $this->fetch("sites");
+        $cache = array(
+            'sites' => array_fill_keys($sites, array()), // site > env > domain
+            'domain' => array(), // domain > env, site
+        );
+        foreach ($sites as $site) {
+            $envs = $this->fetch("sites/{$site}/envs");
+            foreach ($envs as $env) {
+                $env = $env['name'];
+                $cache['sites'][$site][$env] = array();
+                $domains = $this->fetch("sites/{$site}/envs/{$env}/domains");
+                foreach ($domains as $domain) {
+                    $domain = $domain['name'];
+                    $cache['sites'][$site][$env][$domain] = $domain;
+                    $cache['domains'][$domain] = array(
+                        'alias' => '',
+                        'env' => $env,
+                        'site' => $site,
+                    );
+                }
+            }
+        }
+        $fp = fopen($this->cache_file, 'w');
+        fwrite($fp, json_encode($cache));
+        fclose($fp);
+        print "Cache file updated\n";
+    }
 
-	protected function reset($args = array()) {
-		$sites = $this->fetch("sites");
-		$cache = array(
-			'sites' => array_fill_keys($sites, array()), // site > env > domain
-			'domain' => array(), // domain > env, site
-		);
-		foreach($sites as $site) {
-		        $envs = $this->fetch("sites/{$site}/envs");
-		        foreach ($envs as $env) {
-		               $env = $env['name'];
-		               $cache['sites'][$site][$env] = array();
-		               $domains = $this->fetch("sites/{$site}/envs/{$env}/domains");
-		               foreach ($domains as $domain) {
-		                      $domain = $domain['name'];
-				      $cache['sites'][$site][$env][$domain] = $domain;
-				      $cache['domains'][$domain] = array(
-					      'alias' => '',
-					      'env' => $env,
-					      'site' => $site,
-				      );
-		               }
-		       }
-		}
-		$fp = fopen($this->cache_file, 'w');
-		fwrite($fp, json_encode($cache));
-		fclose($fp);
-		print  "Cache file updated\n";
-	}
+    protected function view($arg = '') {
+        var_dump($this->cache);
+    }
 
-	protected function view($arg = '') {
-		var_dump($this->cache);
-	}
+    protected function varnish($arg) {
+        // curl -s -u "${username}:${token}" -X DELETE ${endpoint}sites/${site}/envs/${env}/domains/${domain}/cache.json
+        $domain = $this->_parse_domain($arg);
+        print "Clear varnish for {$domain}\n";
+        if (!isset($this->cache['domains'][$domain])) {
+            print "Missing domain {$domain}\n";
+            return;
+        }
 
-	protected function varnish($arg) {
-		// curl -s -u "${username}:${token}" -X DELETE ${endpoint}sites/${site}/envs/${env}/domains/${domain}/cache.json
-		$domain = $arg;
-		print "Clear varnish for {$domain}\n";
-		if (!isset($this->cache['domains'][$domain])) {
-			print "Missing domain {$domain}\n";
-			return;
-		}
+        $site = $this->cache['domains'][$domain]['site'];
+        $env = $this->cache['domains'][$domain]['env'];
+        curl_setopt($this->curl, CURLOPT_CUSTOMREQUEST, "DELETE");
+        $result = $this->fetch("sites/${site}/envs/${env}/domains/${domain}/cache");
+    }
 
-		$site = $this->cache['domains'][$domain]['site'];
-		$env = $this->cache['domains'][$domain]['env'];
-		curl_setopt($this->c, CURLOPT_CUSTOMREQUEST, "DELETE");
-		$result = $this->fetch("sites/${site}/envs/${env}/domains/${domain}/cache");
-	}
 }
-
